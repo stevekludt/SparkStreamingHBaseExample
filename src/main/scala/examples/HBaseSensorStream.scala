8e95a265ad28d06dd5f469d82341f44c0204f39e
stevekludt <stevekludt@gmail.com>
2016-01-28 14:36:28 -0600

    working with EventHub to Hbase

--- src/main/scala/examples/HBaseSensorStream.scala
+++ src/main/scala/examples/HBaseSensorStream.scala
@@ -5,29 +5,17 @@
 
 package examples
 
-import org.apache.hadoop.hbase.HBaseConfiguration
-import org.apache.hadoop.hbase.client.Put
-import org.apache.hadoop.hbase.io.ImmutableBytesWritable
-import org.apache.hadoop.hbase.mapred.TableOutputFormat
+import java.util.Properties
 import org.apache.hadoop.hbase.util.Bytes
-import org.apache.hadoop.mapred.JobConf
+import kafka.producer.{KeyedMessage, ProducerConfig, Producer}
 import org.apache.spark.SparkConf
 import it.nerdammer.spark.hbase._
-import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.Seconds
 import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.eventhubs.EventHubsUtils
 
 object HBaseSensorStream {
   final val tableName = "sensor"
-  final val cfDataBytes = Bytes.toBytes("data")
-  final val cfAlertBytes = Bytes.toBytes("alert")
-  final val colHzBytes = Bytes.toBytes("hz")
-  final val colDispBytes = Bytes.toBytes("disp")
-  final val colFloBytes = Bytes.toBytes("flo")
-  final val colSedBytes = Bytes.toBytes("sedPPM")
-  final val colPsiBytes = Bytes.toBytes("psi")
-  final val colChlBytes = Bytes.toBytes("chlPPM")
 
   val ehParams = Map[String, String](
     "eventhubs.policyname" -> "Spark",
@@ -40,7 +28,7 @@ object HBaseSensorStream {
     "eventhubs.checkpoint.interval" -> "10"
   )
 
-  // schema for sensor data   
+  // schema for sensor data
   case class Sensor(deviceID: String, date: String, time: String, temp: Double, humid: Double, flo: Double, co2: Double, psi: Double, chlPPM: Double)
 
   object Sensor {
@@ -62,53 +50,70 @@ object HBaseSensorStream {
 
       return (rowkey, sensor.temp, sensor.humid, sensor.flo, sensor.co2, sensor.psi, sensor.chlPPM)
     }
-
-    // convert psi alert to an HBase put object
-    def convertToPutAlert(sensor: Sensor): (ImmutableBytesWritable, Put) = {
-      val dateTime = sensor.date + " " + sensor.time
-      // create a composite row key: sensorid_date time
-      val key = sensor.deviceID + "_" + dateTime
-      val p = new Put(Bytes.toBytes(key))
-      // add to column family alert, column psi data value to put object 
-      p.add(cfAlertBytes, colPsiBytes, Bytes.toBytes(sensor.psi))
-      return (new ImmutableBytesWritable(Bytes.toBytes(key)), p)
-    }
   }
 
-  def main(args: Array[String]): Unit = {
-    // set up HBase Table configuration
-    //val conf = HBaseConfiguration.create()
-    //conf.set(TableOutputFormat.OUTPUT_TABLE, tableName)
-    //val jobConfig: JobConf = new JobConf(conf, this.getClass)
-    //jobConfig.set("mapreduce.output.fileoutputformat.outputdir", "/home/stevekludt/out")
-    //jobConfig.setOutputFormat(classOf[TableOutputFormat])
-    //jobConfig.set(TableOutputFormat.OUTPUT_TABLE, tableName)
+  def createContext() : StreamingContext = {
+    val driverPort = 7777
+    val sparkConf = new SparkConf()
+      .setAppName("DatuhStreaming")
+      .set("spark.logConf", "true")
+      .set("spark.driver.port", driverPort.toString)
+      .set("spark.akka.logLifecycleEvents", "true")
 
-    val sparkConf = new SparkConf().setAppName("HBaseStream")
     // create a StreamingContext, the main entry point for all streaming functionality
-    val ssc = new StreamingContext(sparkConf, Seconds(10))
+    val ssc = new StreamingContext(sparkConf, Seconds(2))
+
     // parse the lines of data into sensor objects
-    //val sensorDStream = ssc.textFileStream("/home/stevekludt/stream").map(Sensor.parseSensor)
-    //sensorDStream.print()
-    val sensorDStream = EventHubsUtils.createUnionStream(ssc, ehParams).map(s => Bytes.toString(s)).map(Sensor.parseSensor)
+    val sensorDStream = EventHubsUtils.createUnionStream(ssc, ehParams)
+      .map(s => Bytes.toString(s))
+      .map(Sensor.parseSensor)
+
+    //this next line is for testing
     sensorDStream.print()
 
     sensorDStream.foreachRDD { rdd =>
       // filter sensor data for low psi
       val alertRDD = rdd.filter(sensor => sensor.psi < 5.0)
-      alertRDD.take(1).foreach(println)
-      // convert sensor data to put object and write to HBase table column family data
-      rdd.map(Sensor.convertForHBase).
-        toHBaseTable(tableName)
+
+      // convert sensor data to for use by nerdammer and write to HBase table column family data
+      rdd.map(Sensor.convertForHBase)
+        .toHBaseTable(tableName)
         .toColumns("temp", "humidity", "flo", "co2", "psi", "chlPPM")
         .inColumnFamily("data")
         .save()
-      //print to the terminal
-      rdd.map(Sensor.convertForHBase).take(100).foreach(println)
-      // convert alert data to put object and write to HBase table column family alert
-      /**alertRDD.map(Sensor.convertToPutAlert).
-        saveAsHadoopDataset(jobConfig)*/
+
+      //Send to Kafka
+          //TODO: need to use a producer pool for kafka
+          //example can be found at: www.michael-noll.com/blog/2014/10/01/kafka-spark-streaming-integration-example-tutorial
+      rdd.foreachPartition(partitionOfRecords => {
+        val topic = "spark"
+        val brokers = "localhost:9092"
+        val props = new Properties()
+        props.put("metadata.broker.list", brokers)
+        props.put("serializer.class", "kafka.serializer.StringEncoder")
+        props.put("producer.type", "async")
+        val config = new ProducerConfig(props)
+        val producer = new Producer[String, String](config)
+
+        partitionOfRecords.foreach { e =>
+          //should try to convert the message to an json objec here before sending
+          val event = new KeyedMessage[String, String](topic, e.deviceID, e.toString)
+          producer.send(event)
+        }
+        producer.close()
+      })
     }
+    ssc
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    // Get StreamingContext from checkpoint directory or create a new one
+    val ssc = StreamingContext.getOrCreate(ehParams("eventhubs.checkpoint.dir"),
+      () => {
+        createContext()
+      })
+
     // Start the computation
     ssc.start()
     // Wait for the computation to terminate
