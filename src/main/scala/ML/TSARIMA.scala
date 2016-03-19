25293740cb1d0be2163acf29ba3fd9ea8d6094d2
stevekludt <stevekludt@gmail.com>
2016-03-19 01:00:10 -0500

    Time Series Arima model working in batch

--- src/main/scala/ML/TSARIMA.scala
+++ src/main/scala/ML/TSARIMA.scala
@@ -2,13 +2,15 @@ package ML
 
 import java.time.{LocalDateTime, ZoneId, ZonedDateTime}
 import com.github.nscala_time.time.Imports._
-import org.apache.spark.sql.SQLContext
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkContext, SparkConf}
 import sparkStream.HBaseRead
 import com.cloudera.sparkts._
+import com.cloudera.sparkts.models.ARIMA
 import com.cloudera.sparkts.stats.TimeSeriesStatisticalTests
 import org.apache.spark.sql.{DataFrame, Row, SQLContext}
 import org.apache.spark.sql.functions._
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
 
 /**
   * Created by stevekludt on 3/17/16.
@@ -34,23 +36,32 @@ object TSARIMA {
 
     val zone = ZoneId.systemDefault()
     val dtIndex = DateTimeIndex.uniformFromInterval(
-      ZonedDateTime.of(LocalDateTime.parse("2016-03-08T00:00:00"), zone),
-      ZonedDateTime.of(LocalDateTime.parse("2016-03-17T00:00:00"), zone),
-      new MinuteFrequency(1))
+      ZonedDateTime.of(LocalDateTime.parse("2016-03-14T09:00:00"), zone),
+      ZonedDateTime.of(LocalDateTime.parse("2016-03-14T09:20:00"), zone),
+      new SecondFrequency(1))
 
     val sensorTsrdd = TimeSeriesRDD.timeSeriesRDDFromObservations(dtIndex, dfWithTS,
       "TS", "Name", "temp")
 
     sensorTsrdd.cache()
 
-    println(sensorTsrdd.count())
+    //println(sensorTsrdd.count())
+    val filled: TimeSeriesRDD[String] = sensorTsrdd.fill("linear").removeInstantsWithNaNs()
 
-    sensorTsrdd.foreach(println(_))
+    val ts = filled.toDF().select("_2").map { row =>
+      val toRemove = "[]".toSet
+      val darray = row.toString.filterNot(toRemove).split(",").map(_.toDouble)
+      Vectors.dense(darray)
+    }
+    //val ts2 = ts.select(ts("_2")).rdd
+    val ARIMAModels = ts.foreach{ vector =>
+      val arimaModel = ARIMA.autoFit(vector)
+      println("coefficients: " + arimaModel.coefficients.mkString(","))
+      val forecast = arimaModel.forecast(vector, 20)
+      println("forecast of next 20 observations: " + forecast.toArray.mkString(","))
 
-    val filled = sensorTsrdd.fill("linear")
-
-    filled.foreach(println(_))
-
+    }
+    println("Here are the models: " + ARIMAModels)
   }
 
 }
